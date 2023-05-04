8492f8b3219fdb8a825d07f9aa14740275007fdd
Prernasingh24 <ps2432000@gmail.com>
2023-05-04 21:57:24 +0530

    adding file in b2

---
+++ BreakExample.java
@@ -0,0 +1,18 @@
+package day4_loops;
+//wap to run for loop 10 times 
+//if value of x=5 then break the loop
+public class BreakExample {
+
+	public static void main(String[] args) {
+		
+		for( int x=1;x<10;x++)
+		{
+		    	
+			System.out.println(x);
+			if(x==5)
+		    	break;
+		}
+
+	}
+
+}
