e452d076d0590c3fb2fde5e68920ece36fe50ebe
Prernasingh24 <ps2432000@gmail.com>
2023-05-04 20:16:11 +0530

    adding file

---
+++ Calculate.java
@@ -0,0 +1,29 @@
+package day1;
+//wap to print sum of two numbers
+
+//syntax --> syntax error 
+
+//source code --> compile --> if no error -->  generate .class file 
+//One.java --> One.class [bytecode]
+//byte code -- is machine independent code 
+//plateform independent 
+// filename must be = public class name 
+// max 1 public class is allowed per source code 
+
+//public class pqr{}  //two classes cannot be public 
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		//int - datatype 
+		// a - variable name 
+		
+		int a=10;
+		int b=20;
+		int c= a+b;
+		
+		System.out.println(" sum ="+ c);
+
+	}
+
+}
